f6f92c6d892dc85c19f6f0a8cab706f2a31fe426
Jude Paul <63412160+Jude-Paul@users.noreply.github.com>
2023-08-02 01:17:59 +0400

    Fixed logical and techinal bugs

--- purerecon.sh
+++ purerecon.sh
@@ -3,42 +3,76 @@
 
 main(){
 
+
+
 port_Shot(){
   target=$1
   echo -e "${light_yellow} [+] Scaning for open ports ${clr}"
   rustscan -a $target -p 20,21,22,23,25,53,80,110,111,135,139,143,443,445,993,995,1723,3306,3389,5900,8080 -- -sC -sV -oA $pwd/rootDomains/$domain/nmap
   sleep 2
   echo -e "${light_yellow} [+] Taking screen shots of open ports ${clr}"
-  gowitness --disable-db nmap -f $pwd/rootDomains/$domain/nmap.xml -P screenshots
+  gowitness --disable-db nmap -f $pwd/rootDomains/$domain/nmap.xml -P $pwd/rootDomains/$domain/screenshots
   sleep 1
 } 
 
-subdomains(){
+
+append_unique() {
+
+    # Assign the arguments to variables for readability
+    local source_file="$1"
+    local target_file="$2"
+
+    # Create a temporary file to store the result
+    local temp_file=$(mktemp)
+
+    # Copy the contents of the target file to the temporary file
+    cat "$target_file" > "$temp_file"
+
+    # Read the source file line by line
+    while read -r line; do
+        # Check if the line is already present in the target file
+        if ! grep -Fxq "$line" "$temp_file"; then
+            # If the line is not present, append it to the temporary file
+            echo "$line" >> "$temp_file"
+        fi
+    done < "$source_file"
+
+    # Output the result
+    cat "$temp_file"
+
+    # Clean up the temporary file
+    rm "$temp_file"
+}
+
+subdomains() { 
   domain="$1"
-  if [ ! -d "rootDomains" ]; then
-		mkdir -p rootDomains/$domain/screenshots
     
 
 		pwd=$(pwd)
 		echo -e "${cyan}[+] Gathering subdomains${clr}" 
     echo -e "${turquoise}[+] Trying to collect subdomains from crtsh${clr}" 
 		crtsh -d $domain >> $pwd/rootDomains/$domain/crtsh.txt 
+    echo $pwd/rootDomains/$domain 
+    echo "the above is path"
+    sleep 2
     if [ ! -f "$pwd/rootDomains/$domain/crtsh.txt" ]; then 
     echo -e "${light_red}[!] Error - Failed to collect subdomains from crtsh${clr}"
     fi
-		sleep 1
+		sleep 2
 
     echo -e "${turquoise}[+] Trying to collect subdomains from assetfinder${clr}" 
 		assetfinder --subs-only $domain >> $pwd/rootDomains/$domain/assetfinder.txt 
     if [ ! -f "$pwd/rootDomains/$domain/assetfinder.txt" ]; then 
     echo -e "${light_red}[!] Error - Failed to collect subdomains from assetfinder${clr}"
     fi
+    sleep 2
     
     echo -e "${turquoise}[+] Trying to collect subdomains from sublist3r${clr}"
-    sublist3r -d $domain -o $pwd/rootDomains/$domain/sublister.txt > /dev/null 2>&1
+    subfinder -d $domain -o $pwd/rootDomains/$domain/sublister.txt > /dev/null 2>&1
     if [ ! -f "$pwd/rootDomains/$domain/sublister.txt" ]; then 
     echo -e "${light_red}[!] Error - Failed to collect subdomains from sublist3r${clr}"
     fi 
+    sleep 2
 
     echo -e "${turquoise}[+] Trying to collect subdomains from findomain${clr}"
 		findomain -t $domain -u $pwd/rootDomains/$domain/findomain.txt > /dev/null 2>&1 
@@ -48,7 +82,7 @@ subdomains(){
 		sleep 1
 
     echo -e "${turquoise}[+] Trying to collect subdomains from amass${clr}"       
-    #amass enum -d $domain -o $pwd/rootDomains/$domain/amass.txt > /dev/null 2>&1 
+    timeout 10m amass enum -d $domain -o $pwd/rootDomains/$domain/amass.txt > /dev/null 2>&1 
     if [ ! -f "$pwd/rootDomains/$domain/amass.txt" ]; then 
     echo -e "${light_red}[!] Error - Failed to collect subdomains from amass${clr}"
     fi
@@ -58,8 +92,10 @@ subdomains(){
 
 		echo -e "${turquoise}[+] Doing the bash magic${clr}"
 		cat $pwd/rootDomains/$domain/amass.txt $pwd/rootDomains/$domain/assetfinder.txt $pwd/rootDomains/$domain/crtsh.txt $pwd/rootDomains/$domain/findomain.txt $pwd/rootDomains/$domain/sublister.txt >> $pwd/rootDomains/$domain/all.txt
-    cat $pwd/rootDomains/$domain/all.txt | grep "$domain" >> $pwd/rootDomains/$domain/allsubs.txt
+    cat $pwd/rootDomains/$domain/all.txt | grep "$domain" | awk '{print $1}' >> $pwd/rootDomains/$domain/allsubs.txt
 		sort -u $pwd/rootDomains/$domain/allsubs.txt >> $pwd/rootDomains/$domain/sorted-subs.txt
+
+    
 		
     if [ -f "$pwd/rootDomains/$domain/sorted-subs.txt" ]; then 
     echo -e "${light_yellow}[+] Successfully Combained and Sorted the result${clr}"
@@ -69,29 +105,28 @@ subdomains(){
     fi
 
 		
-
-		echo -e "${green}[+] Checking alive targets ${clr}"
-    touch $pwd/rootDomains/$domain/alive.txt
-    output=$(cat $pwd/rootDomains/$domain/sorted-subs.txt | anew -d $pwd/rootDomains/$domain/unique-sub.txt)
-    echo $output
-    echo "this is the output"
-		cat $pwd/rootDomains/$domain/sorted-subs.txt | anew $pwd/rootDomains/$domain/unique-sub.txt
-    sleep 2
-    cat $pwd/rootDomains/$domain/unique-sub.txt | httprobe -prefer-https | sed 's/https\?:\/\///' | sort -u | tee -a $pwd/rootDomains/$domain/alive.txt
+     
+    #port_Shot $output
     sleep 1
-    echo $ouput | httpx -sc -td -title -server | tee -a $pwd/rootDomains/$domain/httpx.txt
-    # sleep 2
-    port_Shot $output
-    sleep 1
-    if [ ! -f "$pwd/rootDomains/$domain/alive.txt" ]; then 
-    echo "${light_yellow}[+] So far everything G00D ${clr}"
-    else
-    echo "${red}[!] Failure - Recon interrupted ${clr}"
-    fi
-
-  fi
+    
+  
 }
 
+
+probe(){
+  domain="$1"
+  echo -e "${green}[+] Checking alive targets ${clr}"
+  cat $pwd/rootDomains/$domain/sorted-subs.txt | httprobe -c 60 -prefer-https | sed 's/https\?:\/\///' | sort -u | tee -a $pwd/rootDomains/$domain/alive.txt
+  sleep 5
+  echo $pwd/rootDomains/$domain/sorted-subs.txt | httpx -sc -td -title -server | tee -a $pwd/rootDomains/$domain/httpx.txt
+  sleep 2
+
+  if [ ! -f "$pwd/rootDomains/$domain/alive.txt" ]; then 
+  echo "${light_yellow}[+] So far everything G00D ${clr}"
+  else
+  echo "${red}[!] Failure - Recon interrupted ${clr}"
+  fi
+}
 
 
 
@@ -115,9 +150,19 @@ read_File(){
   # Read the file line by line and output each domain
   while read -r domain; do
     echo -e "${green}Target: [$current_domain/$total_domains] - $domain ${clr}"
-    subdomains $domain
+    if [ ! -d "rootDomains" ]; then
+		  mkdir -p rootDomains/$domain/screenshots
+      subdomains $domain
+    else
+      subdomains $domain
+    fi
+    
+
+    probe $domain
+
     ((current_domain++))
   done < "$filename"
+
 }
 
 
@@ -162,11 +207,20 @@ if [ -z "$domain" ] && [ -z "$file" ]; then
 fi
 
 if [ -n "$domain" ] && [ "$recon" = true ]; then
-  subdomains $domain
+    if [ ! -d "rootDomains" ]; then
+		  mkdir -p rootDomains/$domain/screenshots
+      subdomains $domain
+    else
+      subdomains $domain
+    fi
+    probe $domain
+    echo -e  "${magenta}Recon Completed${clr}"
+  
 fi
 
 if [ -n "$file" ] && [ "$recon" = true ]; then
   read_File $file
+  echo -e  "${magenta}Recon Completed${clr}"
 fi
 
 # if [ "$recon" = true ]; then
